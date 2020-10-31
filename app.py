379eb50b7ce832a339c5fd7a1614ac9c729d2ec7
jonca3d <jonca3d@gmail.com>
2020-10-31 21:26:41 +0300

    Методы для инициализации БД добавлены в файл app

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import asyncio
 if __name__ == '__main__':
     from aiogram import executor
     from handlers import dp
-
+    from db.init_db import init_db_tables, init_db_basic_data
+    init_db_tables()
+    init_db_basic_data()
     loop = asyncio.get_event_loop()
     executor.start_polling(dp, loop=loop)
