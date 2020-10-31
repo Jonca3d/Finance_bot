5ae6c2c1abfac2f46dbb54b5821985b35b5561ca
jonca3d <jonca3d@gmail.com>
2020-10-31 20:42:33 +0300

    Написаны сырые SQL-запросы для инициализации БД

--- db/init_db.py
+++ db/init_db.py
@@ -37,4 +37,44 @@ def init_db_tables():
                    ');')
     conn.commit()
 
+    cursor.execute('CREATE TABLE IF NOT EXISTS transaction_types('
+                   'id SERIAL PRIMARY KEY,'
+                   'name VARCHAR(40) NOT NULL'
+                   ');')
+    conn.commit()
 
+    cursor.execute('CREATE TABLE IF NOT EXISTS transaction_categories('
+                   'id SERIAL PRIMARY KEY, '
+                   'name VARCHAR(40) NOT NULL, '
+                   'description TEXT, '
+                   'transaction_type INTEGER NOT NULL, '
+                   'FOREIGN KEY("transaction_type") REFERENCES "transaction_types"("id")'
+                   ');')
+    conn.commit()
+
+    cursor.execute('CREATE TABLE IF NOT EXISTS transactions('
+                   'id SERIAL PRIMARY KEY, '
+                   'user_id INTEGER NOT NULL, '
+                   'transaction_category INTEGER NOT NULL, '
+                   'amount INTEGER NOT NULL, '
+                   'timestamp INTEGER NOT NULL, '
+                   'FOREIGN KEY("user_id") REFERENCES "users"("telegram_id"), '
+                   'FOREIGN KEY("transaction_category") '
+                   'REFERENCES "transaction_categories"("id")'
+                   ');')
+    conn.commit()
+    
+    cursor.execute('CREATE TABLE IF NOT EXISTS payment_schedule('
+                   'id SERIAL PRIMARY KEY,'
+                   'account_id INTEGER NOT NULL, '
+                   'payment_date DATE NOT NULL, '
+                   'amount INTEGER NOT NULL'
+                   ');')
+    conn.commit()
+
+
+def init_db_basic_data():
+    cursor = conn.cursor()
+    check_data = None
+
+    cursor.execute('SELECT EXISTS(SELECT*FROM ')
