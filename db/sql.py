34f20459fa1a3a9e5fc28d840b71984c3a30aa20
jonca3d <jonca3d@gmail.com>
2020-11-01 22:06:21 +0300

    Добавлен запрос для добавления нового счета в БД

--- db/sql.py
+++ db/sql.py
@@ -10,7 +10,7 @@ conn = psycopg2.connect(dbname=db.DATABASE,
 class is_check:
 
     @staticmethod
-    def user(user_id):
+    def user(user_id: int):
         cursor = conn.cursor()
         cursor.execute('SELECT EXISTS(SELECT * FROM users WHERE telegram_id = %s);', (user_id,))
         return cursor.fetchone()
@@ -24,3 +24,27 @@ class insert:
         cursor.execute('INSERT INTO users (telegram_id, first_name, last_name) '
                        'VALUES(%s, %s, %s)', (user_id, first_name, last_name))
         conn.commit()
+
+    @staticmethod
+    def account(user_id: int, name: str, account_balance: int, type_account: int, description='') -> None:
+        """
+        :param user_id: ID пользователя в Телеграм
+        :param name: Название счета
+        :param account_balance: Остаток на счете
+        :param type_account: Тип счета (дебетовый или кредитный)
+        :param description: Описание счета
+        :return:
+        """
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO accounts (name, user_id, description, account_balance, type) '
+                       'VALUES(%s, %s, %s, %s, %s)', (name, user_id, description, account_balance, type_account))
+        conn.commit()
+
+
+class fetch_data:
+
+    @staticmethod
+    def accounts(user_id):
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM accounts WHERE user_id = %s', (user_id,))
+        return cursor.fetchall()
