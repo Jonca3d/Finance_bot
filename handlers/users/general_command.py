7945d5b4d4794c4d8be996c3c6cd245f4544c46b
jonca3d <jonca3d@gmail.com>
2020-11-01 16:30:23 +0300

    Добавлен хэндлер statrt для приветствия пользователя и добавления его в БД, если такого пользователя еще не существует

--- handlers/users/general_command.py
+++ handlers/users/general_command.py
@@ -1,5 +1,7 @@
 from aiogram.dispatcher.filters import Command
 from aiogram.types import Message
+
+from db import sql
 from loader import dp
 
 
@@ -11,3 +13,11 @@ async def show_test(msg: Message):
 @dp.message_handler(Command('start'))
 async def process_start(msg: Message):
     await msg.answer(text='Добро пожаловать в Finance Bot')
+    user_exists = sql.is_check.user(msg.from_user.id)[0]
+    if user_exists:
+        await msg.answer(text=f'С возвращением {msg.from_user.first_name}.\n'
+                              f'Для вызова справки введите /help')
+    else:
+        await msg.answer(text=f'{msg.from_user.first_name}, приятно познакомиться. \n'
+                              f'Для вызова справки введите /help')
+        sql.insert.user(msg.from_user.id, msg.from_user.first_name, msg.from_user.last_name)
