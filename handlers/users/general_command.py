82e10f858993c72a2f278b190ede515ab0b6e1b9
jonca3d <jonca3d@gmail.com>
2020-11-01 17:37:30 +0300

    Добавлен хэнделр options для вызова меню опций бота

--- handlers/users/general_command.py
+++ handlers/users/general_command.py
@@ -21,3 +21,26 @@ async def process_start(msg: Message):
         await msg.answer(text=f'{msg.from_user.first_name}, приятно познакомиться. \n'
                               f'Для вызова справки введите /help')
         sql.insert.user(msg.from_user.id, msg.from_user.first_name, msg.from_user.last_name)
+
+
+@dp.message_handler(Command('help'))
+async def show_help_message(msg: Message):
+    # TODO Придумать текст сообщения
+    #  вынести сообщение в отдельный файл
+    await msg.answer(text='help')
+
+
+@dp.message_handler(Command('options'))
+async def show_options(msg: Message):
+    """
+    Открыть опции бота.
+    Если пользователя не существует в БД, то сначала добавляется пользователь, а потом открывается список опций
+    :param msg:
+    :return:
+    """
+
+    # Проверка существует ли пользователь в БД
+    if not sql.is_check.user(msg.from_user.id)[0]:
+        sql.insert.user(msg.from_user.id, msg.from_user.first_name, msg.from_user.last_name)
+
+    
