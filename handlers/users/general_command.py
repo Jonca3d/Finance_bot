8fc89970deda74a6eaff3fc087fa64006fc7f323
jonca3d <jonca3d@gmail.com>
2020-11-08 22:29:40 +0300

    Добавлена проверка: при попытке добавить транзакцию, если у пользователя нет ни одного счета, то ему предлагается добавить вначале хотябы один счет

--- handlers/users/general_command.py
+++ handlers/users/general_command.py
@@ -1,5 +1,7 @@
+import time
+
 from aiogram.dispatcher.filters import Command
-from aiogram.types import Message, CallbackQuery
+from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
 
 from keyboards.inline.options import inline_options
 from db import sql
@@ -8,7 +10,7 @@ from loader import dp
 
 @dp.message_handler(Command('test'))
 async def show_test(msg: Message):
-    await msg.answer(text='Теысовое сообщение')
+    await msg.answer(text='Тестовое сообщение')
 
 
 @dp.message_handler(Command('start'))
@@ -21,7 +23,7 @@ async def process_start(msg: Message):
     else:
         await msg.answer(text=f'{msg.from_user.first_name}, приятно познакомиться. \n'
                               f'Для вызова справки введите /help')
-        sql.insert.user(msg.from_user.id, msg.from_user.first_name, msg.from_user.last_name)
+        sql.insert.user(msg.from_user.id, msg.from_user.first_name, msg.from_user.last_name, int(time.time()))
 
 
 @dp.message_handler(Command('help'))
@@ -42,11 +44,36 @@ async def show_options(msg: Message):
 
     # Проверка существует ли пользователь в БД
     if not sql.is_check.user(msg.from_user.id)[0]:
-        sql.insert.user(msg.from_user.id, msg.from_user.first_name, msg.from_user.last_name)
+        sql.insert.user(msg.from_user.id, msg.from_user.first_name, msg.from_user.last_name, int(time.time()))
 
     await msg.answer(text='General options',
                      reply_markup=inline_options,
                      parse_mode='HTML')
+
+
+@dp.message_handler(Command('transaction'))
+async def transaction(msg: Message):
+    """
+    При вызове этого хэгдлера предлагается выбрать тип транзакции:
+    Приход-поступление средств
+    Расход-какие либо траты
+    Перевод между счетами-когда мы хотим перевести деньги с одного счета на другой
+    :param msg:
+    :return:
+    """
+    if not sql.is_check.user(msg.from_user.id)[0]:
+        sql.insert.user(msg.from_user.id, msg.from_user.first_name, msg.from_user.last_name, int(time.time()))
+
+    if sql.is_check.account(msg.from_user.id)[0]:
+        inline_transaction_btn = InlineKeyboardMarkup()
+        inline_transaction_btn.add(InlineKeyboardButton(text='Приход', callback_data='transaction:arrival_of_money'))
+        inline_transaction_btn.add(InlineKeyboardButton(text='Расход', callback_data='transaction:spending_of_money'))
+        inline_transaction_btn.add(InlineKeyboardButton(text='Перевод между счетами',
+                                                        callback_data='transaction:transfer_between_accounts'))
+        inline_transaction_btn.add(InlineKeyboardButton(text='Отмена', callback_data='cancel_menu'))
+        await msg.answer(text='Выберите тип транзакции', reply_markup=inline_transaction_btn)
+    else:
+        await msg.answer(text='У вас пока нет ни одного счета. Введите /options и добавте хотябы один счет')
 
 
 @dp.callback_query_handler(text_contains='cancel_menu')
